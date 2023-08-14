79b237de7ec54495389d2d94730ce34a47c9a6f9
marianasantiagobarbieri <mariana.barbieri@estudante.ifms.edu.br>
2023-08-13 22:46:29 -0300

    Update connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,7 @@
 const { Sequelize } = require('sequelize');
 
 const sequelize = new Sequelize(
-  "postgres://yxwstqqn:SJNvNiw1xTR-UY2P2VQbszSa7NN0geiu@silly.db.elephantsql.com/yxwstqqn", { //banco de dados "ptas-2" email mariaeduardaestudante
+  "postgres://yxwstqqn:SJNvNiw1xTR-UY2P2VQbszSa7NN0geiu@silly.db.elephantsql.com/yxwstqqn", { 
   define: {
     timetamps: true,
     underscored: true,
@@ -12,7 +12,7 @@ try {
   sequelize.authenticate();
   console.log('Conectado com o ElephantSQL!');
 } catch (error) {
-  console.error('Anteção, a conexão falhou!:', error);
+  console.error('Atenção, a conexão falhou!:', error);
 }
 
 module.exports = { Sequelize, sequelize };
